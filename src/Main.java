dda1856dcb5575b7c23c69b2f5cc158b6dd01aef
Doan Van Hieu <doanhieu.bl.hn@gmail.com>
2019-09-30 21:56:09 +0700

    fan

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Fan fan1 =new Fan();
+        fan1.setSpeed(3);
+        fan1.setRadius(10);
+        fan1.setColor("yellow");
+        fan1.setOn(true);
+        System.out.println("Fan on");
+        fan1.toString();
+        Fan fan2 =new Fan();
+        fan2.setOn(false);
+        fan2.setSpeed(2);
+        fan2.setRadius(5);
+        fan2.setColor("blue");
+        System.out.println("Fan off");
+        fan2.toString();
+
+
+    }
+}
